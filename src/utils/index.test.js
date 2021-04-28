99eee92a3d5f64f433a4cd05938a43356b664b20
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-04-28 23:23:52 +0100

    test: add unit tests for asString

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,4 +1,51 @@
-import { formatTime } from './index'
+import { asString, formatTime } from './index'
+
+describe(`${asString.name} function`, () => {
+  describe('with unexpected inputs', () => {
+    const cases = [
+      ['undefined', 'undefined', 'empty string', undefined, undefined, ''],
+      ['non-numeric string', 'undefined', 'empty string', 'hello', undefined, ''],
+      ['object', 'undefined', 'empty string', {}, undefined, ''],
+      ['non-integer', 'undefined', 'empty string', {}, undefined, ''],
+      ['as string', 'undefined', 'expected output', '5', undefined, '5'],
+      ['as string', 'integer', 'expected output', '5', 2, '05'],
+      ['numeric', 'an object', 'number as string', 5, {}, '5'],
+      ['numeric', 'null', 'number as string', 5, null, '5']
+    ]
+
+    test.each(cases)(
+      'given number is %s & minDigits is %s, returns %s',
+      (_, __, ___, number, minDigits, expected) => {
+        const result = asString(number, minDigits)
+
+        expect(result).toBe(expected)
+      }
+    )
+  })
+
+  describe('with expected inputs', () => {
+    const cases = [
+      [4, undefined, '4'],
+      [4, 1, '4'],
+      [4, 2, '04'],
+      [42, undefined, '42'],
+      [42, 0, '42'],
+      [42, 1, '42'],
+      [42, 2, '42'],
+      [42, 3, '042'],
+      [88, 6, '000088']
+    ]
+
+    test.each(cases)(
+      'given number is %s & minDigits is %s, returns %s',
+      (number, minDigits, expected) => {
+        const result = asString(number, minDigits)
+
+        expect(result).toBe(expected)
+      }
+    )
+  })
+})
 
 describe(`${formatTime.name} function`, () => {
   describe('with unexpected inputs', () => {
