eadc0877f84d966cc1eab442fb9dc0fe67355aa5
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2022-02-28 13:57:16 +0000

    test: add collectCoverageFrom to jest config

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,7 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!src/**/*.d.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
